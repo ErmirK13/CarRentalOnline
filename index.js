5c47d1de4b43380fe8cccc3529be109c79d21df9
amirb63 <ab74926@ubt-uni.net>
2025-12-17 09:41:30 +0100

    FinishLOginForm

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ document.getElementById("menuToggle").onclick = function () {
   var nav = document.getElementById("mainNav");
   nav.classList.toggle("active");
 };
+const SignUp = document.getElementById("SignUp");
+const SignIn = document.getElementById("SignIn");
+
+
+document.querySelector(".register-btn").addEventListener("click", () => {
+    SignUp.style.display = "flex";
+});
+
+
+document.getElementById("toSignIn").addEventListener("click", () => {
+    SignUp.style.display = "none";
+    SignIn.style.display = "flex";
+});
+
+
+document.getElementById("toSignUp").addEventListener("click", () => {
+    SignIn.style.display = "none";
+    SignUp.style.display = "flex";
+});
+
+
+window.addEventListener("click", (e) => {
+    if (e.target === SignUp) SignUp.style.display = "none";
+    if (e.target === SignIn) SignIn.style.display = "none";
+});
